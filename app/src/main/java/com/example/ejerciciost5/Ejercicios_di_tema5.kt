c1b043e3d1f5e7e5d3e51679f60f0a0da3265aa7
memnochdavid <davduquediaz@gmail.com>
2025-01-09 17:48:01 +0100

    asdasdaf

---
+++ app/src/main/java/com/example/ejerciciost5/Ejercicios_di_tema5.kt
@@ -0,0 +1,19 @@
+package com.example.ejerciciost5
+
+//ejercicio 1
+/*1. Escribe una función que tome una cadena
+como parámetro e imprima su longitud.*/
+
+fun longi_cadena(cadena:String): Int {
+    return cadena.length
+}
+
+//ejercicio 2
+/*2. Comparación de Cadenas: Crea dos cadenas y
+utiliza el método equals para determinar si
+son iguales.*/
+
+fun compara_cadenas(cad1:String, cad2:String):Boolean{
+    return cad1.equals(cad2)
+}
+
